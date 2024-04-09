1c3e29a27cbfaa15b3a0ee3038f0dce1c3118b7c
Akash Dewangan <103268574+akash-dewangan03@users.noreply.github.com>
2024-04-09 17:50:38 +0530

    Add files via upload

---
+++ Minimum_Integer/Minimum_Integer.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define dob double
+#define cout(x) cout<<x<<endl
+#define vec(type1, name) vector<type1> name
+#define vecp(type1, type2, name) vector<pair<type1, type2>> name
+#define set(type, name) set<type> name
+#define map(type1, type2, name) map<type1, type2> name
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define Pi 3.1415926535897932384626
+#define xin() for(ll i=0;i<n;++i) cin>>x[i]
+#define xout() for(ll i=0;i<n;++i) cout<<x[i]<<" "
+#define forl(i,a,b) for(ll i=a;i<b;++i)
+#define forll(i,a,b) for(ll i=a;i<=b;++i)
+// factorial | lcm | palindrome | power | prime | prime factor | sieveprime
+
+void solve()
+{
+    string s;
+    cin>>s;
+    ll k; cin>>k;
+    ll n = s.size();
+    stack<ll>stc;
+    string str="";
+    if(k>=n) {cout("0"); return;}
+    if(k==0) { cout(s); return;}
+
+    for(ll i=0; i<n; ++i){
+        while(!stc.empty() and k>0 and stc.top()>s[i]){
+            stc.pop();
+            k--;
+        }
+        stc.push(s[i]);
+        if(stc.size()==1 and s[i]=='0') stc.pop();
+    }
+
+
+    while(!stc.empty() and k>0){
+        stc.pop();
+        k--;
+    }
+
+    while(!stc.empty()){
+        str+=stc.top();
+        stc.pop();
+    }
+
+    reverse(str.begin(), str.end());
+
+    if(!str.size()) {cout("0");}
+    else cout(str);
+
+
+
+  return;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ll T = 1;
+//   cin >> T;
+  while(T--){
+     solve();
+  }
+}
